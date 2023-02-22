453fb98e2bd118bf7e3cfee7d5b2619285a76e60
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-02-22 12:22:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 value ="project-name" data-ns-test >
+       Name 
+      </h1>
+      <p value = "project-description" data-ns-test >
+        Description 
+      </p>
+    </div>
   )
 }
 
